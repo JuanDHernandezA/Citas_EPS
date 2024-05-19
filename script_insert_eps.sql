056072b8d2f0c5138048c7c0cc42c05d8b3db70a
JDiegoHernandezA <jdiegohernandeza@gmail.com>
2024-05-19 12:33:53 -0500

    Se agrega Jornada a los modelos

--- script_insert_eps.sql
+++ script_insert_eps.sql
@@ -32,6 +32,9 @@ INSERT INTO consultorio(numero_con,especialidad_id,sede_id) values
 
 INSERT INTO administrador VALUES
 	('1234','Admin','EPS','eps@gmail.com','5551234','1980-04-23',1);
+	
+INSERT INTO jornada(nombre_jornada,hora_inicio,hora_fin) VALUES
+	('DIURNA','06:00:00','12:00:00')
 
 SELECT * FROM genero;
 SELECT * FROM estado;
@@ -40,6 +43,7 @@ SELECT * FROM especialidad;
 SELECT * FROM sede;
 SELECT * FROM consultorio;
 SELECT * FROM administrador;
+SELECT * FROM jornada;
 
 SELECT numero_con AS numero_consultorio, nombre_esp AS especialidad, nombre_sede FROM consultorio
 INNER JOIN especialidad ON consultorio.especialidad_id = especialidad.id_esp
